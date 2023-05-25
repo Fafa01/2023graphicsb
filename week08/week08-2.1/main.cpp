136caebe37c120bfff9212b609f75efc0e3c7f62
Fafa01 <10160792@me.mcu.edu.tw>
2023-05-25 16:15:11 +0800

    week08

---
+++ week08/week08-2.1/main.cpp
@@ -0,0 +1,29 @@
+#include <GL/glut.h>
+#include "glm.h"
+GLMmodel * pmodel = NULL;
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    if(pmodel ==NULL){
+        pmodel = glmReadOBJ("Gundam.obj");
+        glmUnitize(pmodel);
+        glmFacetNormals(pmodel);
+        glmVertexNormals(pmodel,90);
+}
+    glmDraw(pmodel,GLM_SMOOTH|GLM_MATERIAL);
+        glutSolidTeapot(0.3);
+        glutSwapBuffers();
+
+
+}
+
+int main(int argc, char *argv[])
+
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("week08");
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+}
